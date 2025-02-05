e59c3a5db2a8b0f97cb61a45b3a54093138a8c96
Junghyeji <arang030@naver.com>
2025-02-06 00:54:29 +0900

    게시글 작성 데이터 전달

--- src/main/java/com/hyeji/board/controller/BoardController.java
+++ src/main/java/com/hyeji/board/controller/BoardController.java
@@ -1,12 +1,25 @@
 package com.hyeji.board.controller;
 
+import com.hyeji.board.dto.BoardDTO;
 import com.hyeji.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
 
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        return "index";
+    }
 }

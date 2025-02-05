33e2d261a73d983aaf5c55565a95bd821c98844c
Junghyeji <arang030@naver.com>
2025-02-06 00:41:47 +0900

    Controller, Service, Repository 클래스 설정

---
+++ src/main/java/com/hyeji/board/controller/BoardController.java
@@ -0,0 +1,12 @@
+package com.hyeji.board.controller;
+
+import com.hyeji.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+}
---
+++ src/main/java/com/hyeji/board/service/BoardService.java
@@ -0,0 +1,12 @@
+package com.hyeji.board.service;
+
+import com.hyeji.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+}

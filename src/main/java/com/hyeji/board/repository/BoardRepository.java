7c25c472e876069d13c310067e18362112eaa63c
Junghyeji <arang030@naver.com>
2025-02-06 11:29:45 +0900

    게시글 목록 출력 메서드 구현

--- src/main/java/com/hyeji/board/repository/BoardRepository.java
+++ src/main/java/com/hyeji/board/repository/BoardRepository.java
@@ -5,15 +5,21 @@ import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class BoardRepository {
     private final SqlSessionTemplate sql;
 
     public void save(BoardDTO boardDTO) {
-        /* board: mapper의 namespace를 가르킴.
+        /* Board: mapper의 namespace를 가르킴.
            save: sql 쿼리문을 담고있는 태그를 의미
            boardDTO: DB에 넘길 파라미터나 변수가 있다면 그 객체나 데이터 */
         sql.insert("Board.save", boardDTO);
+    }
+
+    public List<BoardDTO> findAll() {
+        return sql.selectList("Board.findAll");
     }
 }
